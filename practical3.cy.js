bb214d890e5170a5452d9a371ce4ee9906a19379
Kiruthiga <kiruthiga@qaoncloud.com>
2025-02-06 21:34:21 +0530

    Add files via upload

---
+++ practical3.cy.js
@@ -0,0 +1,64 @@
+// RESTful Booker API Tests
+
+describe('RESTful Booker API Tests', () => {
+
+    let token;
+    let bookingId;
+  
+    it('Create Token', () => {
+      cy.request({
+        method: 'POST',
+        url: 'https://restful-booker.herokuapp.com/auth',
+        body: {
+          username: 'admin',
+          password: 'password123'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        token = response.body.token;
+        expect(token).to.not.be.empty;
+      });
+    });
+  
+    it('Get Booking IDs', () => {
+      cy.request('https://restful-booker.herokuapp.com/booking').then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.be.an('array');
+        bookingId = response.body[0].bookingid;
+      });
+    });
+  
+    it('Get Booking by ID', () => {
+      cy.request(`https://restful-booker.herokuapp.com/booking/${bookingId}`).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('firstname');
+      });
+    });
+  
+    it('Create Booking', () => {
+      cy.request({
+        method: 'POST',
+        url: 'https://restful-booker.herokuapp.com/booking',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: {
+          firstname: 'John',
+          lastname: 'Doe',
+          totalprice: 150,
+          depositpaid: true,
+          bookingdates: {
+            checkin: '2024-02-01',
+            checkout: '2024-02-05'
+          },
+          additionalneeds: 'Breakfast'
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('bookingid');
+        bookingId = response.body.bookingid;
+      });
+    });
+  
+  });
+  
